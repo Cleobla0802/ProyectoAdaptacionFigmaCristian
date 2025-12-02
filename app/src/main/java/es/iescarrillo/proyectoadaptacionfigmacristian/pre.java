427af64066f43ba6f97a7894f9eeb17585d8995b
Cristian <cleobla0802@iescarrillo.es>
2025-12-02 12:05:19 +0100

    commit

---
+++ app/src/main/java/es/iescarrillo/proyectoadaptacionfigmacristian/pre.java
@@ -0,0 +1,18 @@
+package es.iescarrillo.proyectoadaptacionfigmacristian;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class pre extends ComponentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+    }
+}
